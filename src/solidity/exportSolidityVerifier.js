dbd3318d32f718282e9b64900cc1609374ee27fd
RogerTaule <roger.taulee@gmail.com>
2023-06-07 19:29:17 +0200

    Adding interface and optional check inputs

--- src/solidity/exportSolidityVerifier.js
+++ src/solidity/exportSolidityVerifier.js
@@ -13,6 +13,9 @@ module.exports.exportSolidityVerifier = async function exportSolidityVerifier(vk
 
     const xiSeed = options.xiSeed ? true : false;
 
+    const createInterface = options.createInterface ? true : false;
+    const checkInputs = options.checkInputs ? true : false;
+
     for(let i = 0; i < vk.f.length; ++i) {
         if(vk.f[i].stages.length === 1 && vk.f[i].stages[0].stage === 0) {
             vk.f[i].commit = curve.G1.toObject(vk.f[i].commit);
@@ -62,12 +65,21 @@ module.exports.exportSolidityVerifier = async function exportSolidityVerifier(vk
         xiSeed,
         nonCommittedPols,
         extendLoops,
+        checkInputs,
     };
     if (logger) logger.info("FFLONK EXPORT SOLIDITY VERIFIER FINISHED");
 
     const template = await fs.promises.readFile(path.resolve(__dirname, "verifier.sol.ejs"), "utf-8");
 
-    return ejs.render(template, obj); 
+    if(!createInterface) {
+        return ejs.render(template, obj);
+    } else {
+        const templateInterface = await fs.promises.readFile(path.resolve(__dirname, "interface_verifier.sol.ejs"), "utf-8");
+
+        const verifierCode = ejs.render(template, obj);
+        const interfaceVerifierCode = ejs.render(templateInterface, obj);
+        return [ verifierCode, interfaceVerifierCode ]; 
+    }
 
     function toVkey(val) {
         const str = curve.Fr.toObject(val);
